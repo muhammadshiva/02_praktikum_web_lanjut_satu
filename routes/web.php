4d75b48e2b7dd125703153a33d1556da542aab18
muhammadshiva <shivamuhammad07@gmail.com>
2021-02-16 09:20:41 +0700

    Praktikum 1

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return "Selamat Datang";
+});
+    
+Route::get('/about', function () {
+    return "Nim : 1941720005, Nama : M. Shiva Matahari Yusda";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Ini adalah artikel ID " .$id;
+});
+
+Route::get('/articles/{}', function ($id) {
+    return "Ini adalah artikel ID " .$id;
+});
